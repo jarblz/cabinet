7a3ecd5154b85ee02dc615d189c2a0bcc0a1597d
jared <jaredables@gmail.com>
2018-01-28 19:51:22 -0700

    fixing a bug in a migration

--- db/migrate/20180126223513_create_assessment.rb
+++ db/migrate/20180126223513_create_assessment.rb
@@ -2,7 +2,7 @@ class CreateAssessment < ActiveRecord::Migration[5.0]
   def change
     create_table :assessments do |t|
       t.integer :user_id
-      t.integer :status,
+      t.integer :status
       t.integer :ie, default: 0
       t.integer :vn, default: 0
       t.integer :oa, default: 0
